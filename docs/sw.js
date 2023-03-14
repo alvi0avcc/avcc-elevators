8682f2e8c4fd34d883b541f7dda74eeb46d78964
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2023-03-14 15:09:36 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -15,7 +15,18 @@ self.addEventListener("install", (event) => {
       "https://alvi0avcc.github.io/avcc-elevators/static/css/main.93ab147d.css",
       "https://alvi0avcc.github.io/avcc-elevators/static/css/main.93ab147d.css.map",
       "https://alvi0avcc.github.io/avcc-elevators/static/js/787.dc98cd1f.chunk.js",
-      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-all-300-normal.168d6383e73339293ac3.woff"
+      "https://alvi0avcc.github.io/avcc-elevators/static/js/787.dc98cd1f.chunk.js.map",
+      "https://alvi0avcc.github.io/avcc-elevators/static/js/main.278dc0e2.js",
+      "https://alvi0avcc.github.io/avcc-elevators/static/js/main.278dc0e2.js.LICENSE.txt",
+      "https://alvi0avcc.github.io/avcc-elevators/static/js/main.278dc0e2.js.map",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-all-300-normal.168d6383e73339293ac3.woff",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-cyrillic-300-normal.1431d1cef06ad04f5458.woff2",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-cyrillic-ext-300-normal.4777461b144e55145268.woff2",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-greek-300-normal.db2632771401f61463fe.woff2",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-greek-ext-300-normal.35b9d6be04b95f0f0530.woff2"
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-latin-300-normal.c48fb6765a9fcb00b330.woff2",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-latin-ext-300-normal.dc7dcec8e3f654e0ed63.woff2",
+      "https://alvi0avcc.github.io/avcc-elevators/static/media/roboto-vietnamese-300-normal.32fc45a3d1e8ea11fabc.woff2"
     ])
   );
 });
