300d2dfc0249e9b18b5b3f37d1ae1f40472f1b97
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2023-03-14 14:19:11 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,48 +1,20 @@
-const CACHE_NAME_1 = "V1";
-const STATIC_CACHE_URLS = ["/avcc-elevators", "styles.css", "scripts.js"];
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-self.addEventListener("install", event => {  
-  console.log("Service Worker installing.");
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME_1).then(cache => cache.addAll(STATIC_CACHE_URLS))
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/app.js",
+      "/image-list.js",
+      "/star-wars-logo.jpg",
+      "/gallery/bountyHunters.jpg",
+      "/gallery/myLittleVader.jpg",
+      "/gallery/snowTroopers.jpg",
+    ])
   );
 });
-
-self.addEventListener('message', async (event) => {
-    console.log('Got message in the service worker', event);
-  });
-
-  
-  const CACHE_NAME_2 = "V2";
-
-  self.addEventListener("activate", event => {
-    // delete any unexpected caches
-    event.waitUntil(
-      caches
-        .keys()
-        .then(keys => keys.filter(key => key !== CACHE_NAME_2))
-        .then(keys =>
-          Promise.all(
-            keys.map(key => {
-              console.log(`Deleting cache ${key}`);
-              return caches.delete(key);
-            })
-          )
-        )
-    );
-  });
-
-
-self.addEventListener("fetch", event => {
-    // Cache-First Strategy
-    event.respondWith(
-      caches
-        .match(event.request) // check if the request has already been cached
-        .then(cached => cached || fetch(event.request)) // otherwise request network
-        .then(
-          response =>
-            cache(event.request, response) // put response in cache
-              .then(() => response) // resolve promise with the network response
-        )
-    );
-  });
