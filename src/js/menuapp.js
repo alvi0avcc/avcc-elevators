8a6a6e98fd7c43ed89b9dd8fbe0f29947aa518dc
alvi0avcc <127627433+alvi0avcc@users.noreply.github.com>
2023-03-19 19:58:43 +0300

    +

--- src/js/menuapp.js
+++ src/js/menuapp.js
@@ -130,7 +130,7 @@ function ResponsiveAppBar(props) {
               textDecoration: 'none',
             }}
           >
-            <img src="localhost:3000/avcc-elevators/public/favicon.ico"></img>
+            <img src="favicon.ico"></img>
             Elevators
           </Typography>
         </Toolbar>
