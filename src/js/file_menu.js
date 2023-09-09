8642a20d4aa4a46666e8a16ca297e35c2cb8ab82
alvi0avcc <127627433+alvi0avcc@users.noreply.github.com>
2023-09-09 14:10:13 +0300

    clean

--- src/js/file_menu.js
+++ src/js/file_menu.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import {useEffect, useState} from "react";
-import { Button, ButtonGroup, Stack, Divider, Box } from '@mui/material';
+//import React from 'react';
 import { useContext } from 'react';
 import { UpdateContext } from '../Main'
 import { Elevators } from './elevators.js';
 import * as iolocal from './iolocal';
-import { User } from './user';
 import ServerMenu from './server_menu';
 
 
 export default function FileMenuButton(){
 const [update, setUpdate] = useContext(UpdateContext);
-const [updateView, setUpdateView] = React.useState(false);
+//const [updateView, setUpdateView] = React.useState(false);
 
 const handleImport = (e)=>{
     iolocal.FileImport(e.target.files[0]).then( (result) => { Elevators.setElevators = result; setUpdate( !update ) } );
@@ -28,42 +25,49 @@ const handleImport = (e)=>{
         </div>
         <br/>
 
-    <Stack height={25} style={{ margin: 3 }} direction={'row'} justifyContent={'space-between'} >
-        <Stack  spacing={1} direction={'row'} >
-            <Button variant='outlined'
+    <div style={{ display:'flex', justifyContent: 'space-between' }}>
+        <div>
+            <button className='myButton'
                 style={{ width: 60, fontSize: 12}}
                 onClick={ () => ( 
                 Elevators.setElevators =  iolocal.OpenElevator() ,
                 setUpdate( !update )
                 ) }
-            >Open</Button>
-            <Button variant='outlined'
+            >Open</button>
+            <button className='myButton'
                 style={{ width: 60, fontSize: 12}}
                 onClick={ () => ( 
                 iolocal.SaveElevator()
                 )}
-            >Save</Button>
-        </Stack>
+            >Save</button>
+        </div>
         
-        <Stack spacing={1} direction={'row'}  >
+        <div>
 
-            <Button variant='outlined' component="label"
-                style={{ width: 60, fontSize: 12}}
-                >Import
-                <input hidden accept=".json" type="file" onChange={handleImport}/>
-            </Button>
+            <label 
+                for="fileImport"
+                className='myButton'
+                style={{ display: 'inline-block', width: 60, height: 25, fontSize: 12, paddingTop: 5 }}
+                >Import</label>
+            <input
+                id="fileImport"
+                accept=".json"
+                type="file"
+                hidden
+                onChange={handleImport}
+                />
 
-            <Button variant='outlined'
+            <button className='myButton'
                 style={{ width: 60, fontSize: 12}}
                 onClick={ () => (
                 iolocal.ExportWarehouse()
                 ) }
-            >Export</Button>
+            >Export</button>
 
-        </Stack>
-    </Stack>
+        </div>
+
     </div>
-    <Divider/>
+    </div>
     </>
     )
 }
